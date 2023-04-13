c6160eba21a142a4ba209c80523ef3c35c47c3ee
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-04-13 19:54:58 -0300

    SPA concept

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+const routes = {
+  "/": "/pages/home.html",
+  "/universe": "/pages/universe.html",
+  "/exploration": "/pages/exploration.html"
+}
+
+function route(event) {
+  event = event || window.event
+  event.preventDefault()
+
+  window.history.pushState({}, "", event.target.href)
+
+  handle()
+}
+
+function handle() {
+  const { pathname } = window.location
+  const route = routes[pathname] || routes["/"]
+
+  fetch(route)
+    .then(data => data.text())
+    .then(html => {
+      document.querySelector('.app').innerHTML = html
+    })
+}
+
+handle()
